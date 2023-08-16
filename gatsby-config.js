0e96c371820cc4985faac9e9a2283c96d79ada9a
mrjsb0 <jsamuelb00@proton.me>
2023-08-16 17:25:35 -0400

    basic query and metadata structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,34 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `our-services`,
+        // Path to the directory
+        path: `${__dirname}/src/our-services/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
+      },
+    },
+
+  ],
   siteMetadata: {
     title: 'JB Coatings',
     description: 'Detroit based powdercoating service',
-    copyright: 'Copyright JSB 2023'
+    copyright: 'Copyright JSB 2023',
+    contact: 'jbscoatings@gmail.com'
   }
 }
--- src/pages/offered-services/allServices.js
+++ src/pages/offered-services/allServices.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import * as styles from '../../styles/services.module.css'
+import { Link, graphql } from 'gatsby'
 
-export default function allServices() {
+
+export default function allServices({ data }) {
+    console.log(data)
+    const services = data.allServices.nodes
+    const contact  = data.contact.siteMetadata.contact
     return (
         <Layout>
-    <div>
-    <h1>All of our Services</h1>  
-    </div>
-    </Layout>
-    )
+          <div className={styles.servicesStyle}>
+                {services.map(services =>(
+                    <Link to= {"/our-services/" + services.frontmatter.slug} key={services.id}>
+                        <div>
+                            <h4>{services.frontmatter.title}</h4>
+                            <p>{services.frontmatter.stack}</p>
+                        </div>
+                    </Link>
+                    ))}
+                    <h2>Like what you see? email at { contact } for a quote!</h2>
+            </div>
+        </Layout>
+        );
 }
+
+// export page query
+export const query = graphql`
+query services {
+    allServices: allMarkdownRemark {
+      nodes {
+        html
+        frontmatter {
+          title
+          stack
+          slug
+        }
+      }
+    }
+
+    contact: site{
+      siteMetadata {
+        contact
+      }    
+    }
+}
+`
