2c1b51965345bccb9e21c308cda6d19cd6e2019f
mrjsb0 <jsamuelb00@proton.me>
2023-08-17 18:41:01 -0400

    index banner updating

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,28 @@ import Carousel from "./Carousel";
 
 export default function Home({ data }) {
   console.log(data);
-  
-
-
   return (
     <Layout>
+      
       <div className="carousel-container">
         <Carousel autoPlay showThumbs={false} />
       </div>
+      
+      <div className="banner-container">
+  <div className="banner-text">
+    <h1 style={{color: 'black'}}>Trusted brands</h1>
+  </div>
+  <div className="banner-logos">
+    <img src='/logo.jpg' alt="JB Coatings" />
+    <img src='/logo.jpg' alt="JB Coatings" />
+    <img src='/logo.jpg' alt="JB Coatings" />
+  </div>
+</div>
+      
       <div className="content-container">
         <h3 style={{ color: 'black' }}>At JB Coatings, we take immense pride...</h3>
       </div>
+    
     </Layout>
   );
 }
